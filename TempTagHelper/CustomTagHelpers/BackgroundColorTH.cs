ce51b99d76d6fc026bff5e49e81d3cce96ea1372
Nikunj018 <delvadiyanikunj1234@gmail.com>
2022-03-04 10:23:53 +0530

    Add project files.

---
+++ TempTagHelper/CustomTagHelpers/BackgroundColorTH.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace TempTagHelper.CustomTagHelpers
+{
+    [HtmlTargetElement(Attributes = "background-color")]
+    public class BackgroundColorTH : TagHelper
+    {
+        public string BackgroundColor { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Attributes.SetAttribute("class", $"btn btn-{BackgroundColor}");
+        }
+    }
+}
